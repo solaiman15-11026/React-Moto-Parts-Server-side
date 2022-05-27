47ba804c61ea910fc9c0ef4f717b9d079e5a0b65
solaiman15-11026 <samimunna49@gmail.com>
2022-05-27 11:33:44 +0600

    all

--- index.js
+++ index.js
@@ -10,18 +10,34 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unthorization access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Acess' })
+        }
+        req.decoded = decoded;
+        next();
+    })
 
-
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4y4wz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
 async function run() {
     try {
         await client.connect();
-        const partsCollection = client.db('package').collection('data');
+        const partsCollection = client.db('package').collection('parts');
         const placeOrderCollection = client.db('package').collection('placeOrder');
+        const userCollection = client.db('package').collection('users');
+        const reviewCollection = client.db('package').collection('reviews');
+        const paymentCollection = client.db('package').collection('payments');
+
 
         // get all parts
         app.get('/part', async (req, res) => {
@@ -31,7 +47,7 @@ async function run() {
             res.send(parts)
         });
 
-        // get one inventory item
+        // get one parts item
         app.get('/part/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
@@ -39,18 +55,165 @@ async function run() {
             res.send(part);
         })
 
+        // get all user
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users)
+        })
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+
+        })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30d' })
+            res.send({ result, token });
+        })
+
         app.post("/placeorder", async (req, res) => {
             const placeOrder = req.body;
             const result = await placeOrderCollection.insertOne(placeOrder);
             res.send(result);
         });
-        app.get('/placeorder', async (req, res) => {
+        app.get('/placeorder', verifyJWT, async (req, res) => {
             const customerEmail = req.query.email;
-            console.log(customerEmail);
-            const query = { customerEmail: customerEmail };
-            const orders = await placeOrderCollection.find(query).toArray();
+            // console.log(customerEmail);
+            const decodedEmail = req.decoded.email;
+            if (decodedEmail === customerEmail) {
+                const query = { customerEmail: customerEmail };
+                const orders = await placeOrderCollection.find(query).toArray();
+                return res.send(orders);
+            }
+            else {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+
+        })
+        app.get('/placeorder', async (req, res) => {
+            const query = {};
+            const cursor = placeOrderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+        app.get('/review', async (req, res) => {
+            const query = {};
+            const reviews = await reviewCollection.find(query).toArray();
+            res.send(reviews);
+
+        })
+
+        // add a review | Post review
+        app.post("/review", async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        });
+
+        app.post("/part", async (req, res) => {
+            const product = req.body;
+            const result = await partsCollection.insertOne(product);
+            res.send(result);
+        });
+
+        app.get('/placeorder/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const orders = await placeOrderCollection.findOne(query)
             res.send(orders);
         })
+
+        // delete user
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+        // get user info
+        app.get('/userinfo/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            res.send(user);
+        })
+
+        // delete a product
+        app.delete('/part/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await partsCollection.deleteOne(query)
+            res.send(result);
+        })
+
+        // delete order 
+        app.delete('/placeorder/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await placeOrderCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.get("/placeorder/:id", verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const order = await placeOrderCollection.findOne(query);
+            res.send(order);
+        });
+        app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+            const order = req.body;
+            const price = order.placeOrderPrice;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"],
+            });
+            res.send({ clientSecret: paymentIntent.client_secret });
+        });
+        app.patch("/placeorder/:id", verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId,
+                },
+            };
+
+            const updatedOrder = await placeOrderCollection.updateOne(filter, updateDoc);
+            const result = await paymentCollection.insertOne(payment);
+            res.send(updateDoc);
+        });
     }
 
     finally {
@@ -58,7 +221,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
 
 app.get('/', (req, res) => {
     res.send('Hello auto Motors Manufacturer parts')
